92945cb0f04a25acf19fc27aeb1e09d8034754d8
Marcelo Souza <locee_rj@hotmail.com>
2023-03-21 19:07:59 -0300

    projeto jogo da memoria finalizado]

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'beth',
@@ -26,7 +28,8 @@ const checkEndGame= () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if (disabledCards.length == 20) {
-        alert('Parabéns!!! Você ganhou!!!')
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}!!! seu tempo foi: ${timer.innerHTML}!!!`)
     }
 }
 
@@ -105,4 +108,18 @@ const loadGame = () => {
     });
 }
 
-loadGame();
+const startTimer = () => {
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
+
+window.onload = () => {
+    spanPlayer.innerHTML = localStorage.getItem('player');
+    startTimer();
+    loadGame();
+}
+
+
+
